d81cc322a0ffdb8d449c08d87d37d641cdff6935
Oscar Boykin <oscar@stripe.com>
2017-07-31 09:41:23 -0400

    Revert Pure node, add namedFn
    Pure is rather complex and still does not get us to kleisli. By adding a
    namedFn node where the user gives a unique id for each function, we can
    simplify a lot. It is obviously risky, but we already assume Monad
    coherence, so taking functions like pure or flatMap to be stable, is not
    really increasing risk.
    
    We need some kind of clean/invalidation to deal with cases where there
    are real changes to the monad that has already cached output.

--- core/src/main/scala/org/bykn/hyperbuild/Build.scala
+++ core/src/main/scala/org/bykn/hyperbuild/Build.scala
@@ -6,7 +6,7 @@ import java.io.File
 import scala.spores._
 
 sealed trait Build[M[_], +A] {
-  import Build.{Apply, Flatten, /* Cached, CachedOrBuild, Const, Pure, */ Named}
+  import Build.{Apply, Flatten, Cached, CachedOrBuild, Const, Named}
 
   final def mapCached[B: Serialization](fn: Spore[A, B]): Build[M, B] =
     Apply(Build.mod[M].fn[A, B, Spore[A, B]](fn), this).cached
@@ -18,55 +18,35 @@ sealed trait Build[M[_], +A] {
     Flatten[M, B](Apply[M, A, M[B]](Build.mod[M].fn[A, M[B], Spore[A, M[B]]](fn), this)).cached
 
   final def zip[B](that: Build[M, B]): Build[M, (A, B)] = {
-    val withB = Build.mod[M].fn[A, B => (A, B), Build.ZipFn[A, B]](Build.ZipFn[A, B]())
+    val withB = Build.mod[M]
+      .namedFn[A, B => (A, B)](s"${Build.namePrefix}.Build.zipFn") { a =>
+        { b => (a, b) }
+      }
     Apply(Apply(withB, this), that)
   }
 
-  // /**
-  //  * Transform to a new Monad N
-  //  */
-  // final def transform[N[_]](f: M ~> N): Build[N, A] =
-  //   this match {
-  //     case Const(a, hfp) => Const(a, hfp.transform(f))
-  //     case Apply(fn, a) => Apply(fn.transform(f), a.transform(f))
-  //     case p@Pure(_, _) =>
-  //       def go[A0, A1](p: Pure[M, A0, A1]): Build[N, A1] = {
-  //         val leib: Build.Leibniz[A1, M[A0]] = p.eq
-  //         val newL: Build.Leibniz[A1, N[A0]] = ???
-  //         Pure(p.of.transform(f), newL)
-  //         // or:
-  //         // val b: Build[N, N[A0]] = p.of.transform(f).pureBuild
-  //         // type B[T] = Build[N, T]
-  //         // newL.flip.subst[B](b)
-  //       }
-  //       go(p)
-  //     case Flatten(nested) => Flatten(nested.transform(f).map { ma => f(ma) })
-  //     case Cached(inner, ser) => Cached(inner.transform(f), ser)
-  //     case CachedOrBuild(fp, ser, a) => CachedOrBuild(f(fp), ser, () => a().transform(f))
-  //     case Named(inner, name) => Named(inner.transform(f), name)
-  //   }
+  /**
+   * Transform to a new Monad N
+   */
+  final def transform[N[_]](f: M ~> N): Build[N, A] =
+    this match {
+      case Apply(fn, a) => Apply(fn.transform(f), a.transform(f))
+      case Cached(inner, ser) => Cached(inner.transform(f), ser)
+      case CachedOrBuild(fp, ser, a) => CachedOrBuild(f(fp), ser, () => a().transform(f))
+      case Const(a, hfp) => Const(a, hfp.transform(f))
+      case fl@Flatten(_) =>
+        def go[B](flatten: Flatten[M, B]): Build[N, B] = {
+          // this is unsafe because f may not be serializable
+          val inner = Build.mod[N].unsafeFn[M[B], N[B]](f(_))
+          val nestedN: Build[N, M[B]] = flatten.nested.transform(f)
+          Flatten[N, B](inner(nestedN))
+        }
+        go(fl)
+      case Named(inner, name) => Named(inner.transform(f), name)
+    }
 }
 
 object Build {
-
-  /**
-   * Some private serializable and stable functions
-   */
-  private case class ZipFn[A, B]() extends Function1[A, B => (A, B)] with Serializable {
-    def apply(a: A) = { (b: B) => (a, b) }
-  }
-  private case class AndThen[A, B, C]() extends Function1[(A => B, B => C), A => C] with Serializable {
-    def apply(fns: (A => B, B => C)): A => C = fns._1.andThen(fns._2)
-  }
-  private case class ConstFn[A, B]() extends Function1[B, A => B] with Serializable {
-    def apply(b: B) = { (a: A) => b }
-  }
-  private case class ForkFn[A, B, C]() extends Function1[(A => B, A => C), A => (B, C)] with Serializable {
-    def apply(tup: (A => B, A => C)): A => (B, C) = { (a: A) => (tup._1(a), tup._2(a)) }
-  }
-  private case class ApplyFn[A, B]() extends Function1[(A => B, A), B] with Serializable {
-    def apply(tup: (A => B, A)): B = tup._1(tup._2)
-  }
 
   implicit class BuildInvariant[M[_], A](val build: Build[M, A]) extends AnyVal {
     final def cached(implicit ser: Serialization[A]): Build[M, A] = build match {
@@ -81,8 +61,8 @@ object Build {
     }
 
     /** lift the value into the monad M */
-    final def pureBuild: Build[M, M[A]] =
-      Pure(build, implicitly[Build.Leibniz[M[A], M[A]]])
+    final def pureBuild(implicit M: Monad[M]): Build[M, M[A]] =
+      mod[M].namedFn[A, M[A]]("cats.Monad.pure")(M.pure(_))(build)
 
     final def run(memo: Memo[M]): M[(A, Fingerprint)] = {
       import memo.monadError
@@ -107,13 +87,6 @@ object Build {
         of match {
           case Apply(f, a) =>
             cache(f, a, depth, ser)(fn)
-          case p@Pure(_, _) =>
-            def go[B, T1 <: T](p: Pure[M, B, T1]): M[(R, Fingerprint)] = {
-              type Reader[X] = X => M[R]
-              val fnM: M[B] => M[R] = p.eq.subst[Reader](fn)
-              deepFlatten[B, R](depth - 1, p.of, ser) { b: B => fnM(Monad[M].pure(b)) }
-            }
-            go(p)
           case Flatten(inner) =>
             deepFlatten(depth + 1, inner, ser)(_.flatMap(fn))
           case Named(inner, name) =>
@@ -158,14 +131,6 @@ object Build {
             // we have to re-run the function without a caching
             (fab(a), bF)
           }
-        case p@Pure(_, _) =>
-          def go[B, A1 <: A](p: Pure[M, B, A1]): M[(A, Fingerprint)] =
-            for {
-              aFp <- p.of.run(memo)
-              (a, fp) = aFp
-              flatFp = flattenFP(fp, -1) // there is -1 flattening
-            } yield (p.eq.flip.subst[Id](Monad[M].pure(a)), flatFp)
-          go(p)
         case Flatten(m) =>
           for {
             maFp <- m.run(memo)
@@ -188,8 +153,6 @@ object Build {
 
   implicit class BuildM[M[_], A](val build: Build[M, M[A]]) extends AnyVal {
     def flatten: Build[M, A] = Flatten(build)
-    def flatCached(implicit ser: Serialization[A]): Build[M, A] =
-      Cached(Flatten(build), ser)
   }
 
   implicit class BuildFn[M[_], A, B](val fn: Build[M, A => B]) extends AnyVal {
@@ -197,29 +160,32 @@ object Build {
     def apply(a: A)(implicit hfp: HasFingerprint[M, A]): Build[M, B] =
       apply(Const(a, hfp))
 
-    def andThen[C](bfn: Build[M, B => C]): Build[M, A => C] =
-      Apply(Const(AndThen[A, B, C](), HasFingerprint.serializable), fn.zip(bfn))
-  }
-
-  private sealed trait Leibniz[A, B] {
-    def subst[F[_]](f: F[A]): F[B]
-    final def flip: Leibniz[B, A] = {
-      type F[T] = Leibniz[T, A]
-      subst[F](Leibniz.refl[A])
+    def andThen[C](bfn: Build[M, B => C]): Build[M, A => C] = {
+      val atFn = Build.mod[M].namedFn[(A => B, B => C), A => C](
+        s"${Build.namePrefix}.Build.andThen") { case (fn1, fn2) =>
+          fn1.andThen(fn2)
+        }
+      Apply(atFn, fn.zip(bfn))
     }
   }
-  private object Leibniz {
-    implicit def refl[A]: Leibniz[A, A] =
-      new Leibniz[A, A] {
-        @inline def subst[F[_]](f: F[A]): F[A] = f
-      }
+
+  implicit class BuildFnKleisli[M[_], A, B](val fn: Build[M, A => M[B]]) extends AnyVal {
+    def kleisli[C](b: Build[M, B => M[C]])(implicit M: FlatMap[M]): Build[M, A => M[C]] = {
+      val op = mod[M].namedFn[(A => M[B], B => M[C]), A => M[C]](
+        s"${Build.namePrefix}.kleisli") { case (k1, k2) =>
+          { a => k1(a).flatMap(k2) }
+        }
+
+      op(fn.zip(b))
+    }
   }
 
   private case class Apply[M[_], A, B](fn: Build[M, A => B], a: Build[M, A]) extends Build[M, B]
+  // TODO: can probably merge with CachedOrBuild since all Build have a fingerprint
   private case class Cached[M[_], A](b: Build[M, A], ser: Serialization[A]) extends Build[M, A]
+  // TODO: probably want a lazy val to build Build[M, A] so we don't do it repeatedly
   private case class CachedOrBuild[M[_], A](mfp: M[Fingerprint], ser: Serialization[A], a: () => Build[M, A]) extends Build[M, A]
   private case class Const[M[_], A](const: A, fp: HasFingerprint[M, A]) extends Build[M, A]
-  private case class Pure[M[_], A, B](of: Build[M, A], eq: Leibniz[B, M[A]]) extends Build[M, B]
   private case class Flatten[M[_], A](nested: Build[M, M[A]]) extends Build[M, A]
   private case class Named[M[_], A](b: Build[M, A], name: String) extends Build[M, A]
 
@@ -245,8 +211,6 @@ object Build {
       case const@Const(_, _) =>
         def run[B1 <: B](c: Const[M, B1]) = c.fp.fingerprint(c.const)
         run(const)
-      case Pure(of, _) =>
-        go(of).map { n => Fingerprint.combineAll(Fingerprint("pure") :: n :: Nil) }
       case Flatten(nested) => go(nested).map { n => Fingerprint.combineAll(Fingerprint("flatten") :: n :: Nil) }
       case Named(b, _) => go(b) // naming does not change the result
       case Cached(c, _) => go(c) // serialization does not change the values
@@ -260,6 +224,9 @@ object Build {
   private def flattenFP(fp: Fingerprint, depth: Int): Fingerprint =
     Fingerprint.combineAll(Fingerprint(s"flatten/result:$depth") :: fp :: Nil)
 
+  private[hyperbuild] val namePrefix: String =
+    "org.bykn.hyperbuild"
+
   final class Module[M[_]] {
 
     val unit: Build[M, Unit] =
@@ -268,7 +235,7 @@ object Build {
     val None: Build[M, Option[Nothing]] =
       const(scala.None)(HasFingerprint.serializable)
 
-    def noneM[T]: Build[M, M[Option[T]]] =
+    def noneM[T](implicit M: Monad[M]): Build[M, M[Option[T]]] =
       (None: Build[M, Option[T]]).pureBuild
 
     def const[A](a: A)(implicit hfp: HasFingerprint[M, A]): Build[M, A] =
@@ -288,10 +255,12 @@ object Build {
     }
 
     def applyFn[A, B]: Build[M, ((A => B, A)) => B] =
-      fn[(A => B, A), B, ApplyFn[A, B]](ApplyFn())
+      namedFn[(A => B, A), B](s"${Build.namePrefix}.Build.applyFn") { case (fn, a) =>
+        fn(a)
+      }
 
     def constFn[A, B](b: Build[M, B]): Build[M, A => B] =
-      fn[B, A => B, ConstFn[A, B]](ConstFn())(b)
+      namedFn[B , A => B](s"${Build.namePrefix}.Build.zipFn") { b => { a => b } }(b)
 
     private[hyperbuild] def cacheOrBuild[A](mfp: M[Fingerprint], bld: => Build[M, A])(implicit ser: Serialization[A]): Build[M, A] =
       CachedOrBuild(mfp, ser, () => bld)
@@ -299,8 +268,23 @@ object Build {
     def fn[A, B, T <: Function1[A, B] with Serializable](f: T): Build[M, A => B] =
       const[Function1[A, B] with Serializable](f)(HasFingerprint.serializable[M, Function1[A, B] with Serializable])
 
-    def fork[A, B, C](fn1: Build[M, A => B], fn2: Build[M, A => C]): Build[M, A => (B, C)] =
-      fn[(A => B, A => C), A => (B, C), ForkFn[A, B, C]](ForkFn())(fn1.zip(fn2))
+    /**
+     * This requires that any time the function changes, you give it a new
+     * name. If the function changes and then name does not, you will get
+     * build errors
+     */
+    def namedFn[A, B](name: String)(fn: A => B): Build[M, A => B] =
+      constWithFingerprint(fn,
+        Fingerprint.combineAll(Fingerprint(namePrefix + ".namedFn") :: Fingerprint(name) :: Nil))
+
+    def fork[A, B, C](fn1: Build[M, A => B], fn2: Build[M, A => C]): Build[M, A => (B, C)] = {
+      val fork = namedFn[(A => B, A => C), A => (B, C)](
+        s"${Build.namePrefix}.Build.forkFn") { case (f1, f2) =>
+          { a => (f1(a), f2(a)) }
+        }
+
+      fork(fn1.zip(fn2))
+    }
 
     /**
      * Assume we can serialize the function
@@ -323,7 +307,6 @@ object Build {
     def dependencies(b: Build[M, _]): List[String] = b match {
       case Const(a, _) => Nil
       case Apply(fn, a) => dependencies(fn) ++ dependencies(a)
-      case Pure(nested, _) => dependencies(nested)
       case Flatten(nested) => dependencies(nested)
       case Cached(b, _) => dependencies(b)
       case CachedOrBuild(_, _, fn) => dependencies(fn())
